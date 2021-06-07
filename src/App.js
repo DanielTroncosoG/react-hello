dd527e66a7e326315d169df1493e7dd69e7f2a5b
GabrielValdiviaGaboloso <g.valdivia1436@hotmail.com>
2021-06-07 16:07:24 -0400

    card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import Navbar from "./Components/Navbar/Navbar"
-import Jumbotron from "./Components/Jumbotron/Jumbotron"
-import './App.css';
+import React, { isValidElement } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Jumbotron from "./Components/Jumbotron/Jumbotron";
+import Card from "./Components/Card/Card";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="container bg-light">
-      <Jumbotron />
+        <Jumbotron />
+        <div className="row  row-cols-1 row-cols-sm-2 row-cols-lg-4  ">
+          <div className="col-3 ">
+            {" "}
+            <Card />
+          </div>
+          <div className="col-3 ">
+            {" "}
+            <Card />
+          </div>
+          <div className="col-3 ">
+            {" "}
+            <Card />
+          </div>
+          <div className="col-3 ">
+            {" "}
+            <Card />
+          </div>
+        </div>
       </div>
     </div>
   );
-  }
+}
 
 export default App;
