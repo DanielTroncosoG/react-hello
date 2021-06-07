84e6e1fbfabc84fea5b9903df1d185e1509c9bf2
unknown <di.trgonzalez@gmail.com>
2021-06-07 17:47:34 -0400

    commit jumbotron

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './Jumbotron.css';
 
-class Jumbotron extends Component {
-    render() {
-        return(
-            <div className="jumbotron jumbotron-fluid">
-  <h1 className="display-4">A Warm Welcome!</h1>
-  <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ligula sem, vulputate et lectus a, consequat suscipit lectus. Proin facilisis, ante vel commodo tincidunt, nulla erat faucibus leo, sed pharetra tellus dolor malesuada ex. </p>
-  <a className="btn btn-primary btn-lg" href="#" role="button">Call to action</a>
-</div>
-        )
-}
+function Jumbotron({ title, paragraph, button }) {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <h1 className="display-4">{title}</h1>
+            <p className="lead">{paragraph}</p>
+            <a className="btn btn-primary btn-lg" href="#" role="button">{button}</a>
+        </div>
+    )
+
 }
 
 
